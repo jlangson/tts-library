dca90440da0dc8a665c437b9db9dae386a4dbb67
Jared Langson <langson.jared@gmail.com>
2021-02-10 13:25:07 -0500

    Refactor Main.java isBorrowed() -> getBorrowed()

--- src/libraryProject/Main.java
+++ src/libraryProject/Main.java
@@ -15,8 +15,7 @@ public class Main {
 
         // Small test of the Book class
         Book example = new Book("The Da Vinci Code");
-        System.out
-                .println("Title (should be The Da Vinci Code): " + example.getTitle());
+        System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
         System.out.println("Borrowed? (should be false): " + example.isBorrowed());
         example.borrowed();
         System.out.println("Borrowed? (should be true): " + example.isBorrowed());
