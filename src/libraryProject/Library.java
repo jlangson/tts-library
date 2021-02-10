6fb5c0dada02c4965bd9e19d4b34e399e6345180
Jared Langson <langson.jared@gmail.com>
2021-02-10 13:22:31 -0500

    Add method to findBooks

--- src/libraryProject/Library.java
+++ src/libraryProject/Library.java
@@ -1,21 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package libraryProject;
 
-/**
- *
- * @author jared
- */
-public class Library {
+import java.util.ArrayList;
+import java.util.List;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Library {
+//Add the missing implementation to this class
+
+    private String address;
+    private List<Book> catalog;
+
+    public Library() {
     }
-    
+
+    public Library(String address) {
+        this.address = address;
+        this.catalog = new ArrayList<Book>();
+    }
+
+    public void addBook(Book newBook) {
+        catalog.add(newBook);
+    }
+
+    public static void printOpeningHours() {
+        System.out.println("Daily from 9am to 5pm.");
+    }
+
+    public void printAddress() {
+        System.out.println(this.address);
+    }
+
+    public void borrowBook(String title) {
+        Book book = findBookByTitle(title);
+        if(book.getTitle().equals("")){ //book not found
+            System.out.printf("Sorry the book %s is not in our catalog", title);
+        } else if(book.isBorrowed()){
+            System.out.printf("Sorry the book %s is checked out", title);
+        } else{ //
+            System.out.printf("Here is your book %s", title);
+            book.borrowed();
+        }
+        
+    }
+
+    public Book findBookByTitle(String title) {
+        for (Book book : this.catalog) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                //if this used a for loop there would be an index location
+                //so the code could be
+                //return this.catalog.get(i);
+                //and you wouldn't need the int bookLocation = this.catalog.indexOf(book)
+                int bookLocation = this.catalog.indexOf(book);
+                return this.catalog.get(bookLocation);
+            }
+        }
+        return new Book(""); //means book not found
+    }
+
 }
